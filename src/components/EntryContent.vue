3619523d2e036aa01c44710ce3ef5047944d2e56
Morozilka <87670176+IvaMorozilka@users.noreply.github.com>
2024-04-13 16:43:24 +0300

    Update

--- src/components/EntryContent.vue
+++ src/components/EntryContent.vue
@@ -1,9 +1,27 @@
 <script setup>
 import MainContainer from './marking/MainContainer.vue'
+import IconLink from '@/components/icons/IconLink.vue'
 </script>
 
 <template>
-  <MainContainer class="py-36"> </MainContainer>
+  <MainContainer>
+    <div class="flex flex-row justify-between flex-nowrap text-start mb-[30px] py-36">
+      <div class="flex flex-col w-[28%]">
+        <h2 class="text-4xl text-header-1 font-bold">Вы платите только за результат</h2>
+        <h3 class="text-lg text-accent font-normal flex items-center !text-left pt-4">
+          <a href="#" id="link"> Подробнее о компании</a>
+        </h3>
+      </div>
+      <div class="h-full w-[72%]">
+        <p class="text-main-1 font-normal text-xl">
+          Мы взимаем плату за предоставление консалтинговых услуг только после успешного закрытия
+          сделки. Экспертное знание отрасли кредитования юридических лиц, накопленное многолетним
+          опытом работы, позволяет нам быть уверенными в успехе самых сложных случаев привлечения
+          финансирования в юрисдикции РФ
+        </p>
+      </div>
+    </div>
+  </MainContainer>
 </template>
 
 <style scoped></style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,22 +1,38 @@
 <script setup>
 import IconMenu from '@/components/icons/IconMenu.vue'
 import MainContainer from './marking/MainContainer.vue'
-import { ref } from 'vue'
+import { computed, onBeforeMount, ref } from 'vue'
 
-const color = ref('white')
+const color = computed(() => (topOfPage.value ? 'white' : 'black'))
+const topOfPage = ref(true)
 function changeColor() {
   color.value = color.value === 'white' ? 'black' : 'white'
 }
+function handleScroll() {
+  if (window.pageYOffset > 0) {
+    if (topOfPage.value) topOfPage.value = false
+  } else {
+    if (!topOfPage.value) topOfPage.value = true
+  }
+}
+
+onBeforeMount(() => {
+  window.addEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
-  <div id="class-header" class="fixed top-0 left-0 w-full z-10">
+  <div
+    id="class-header"
+    class="fixed top-0 left-0 w-full z-10 transition-all duration-300 ease-out"
+    :class="{ 'bg-white bg-opacity-90': !topOfPage }"
+  >
     <div id="site-header__inner">
       <MainContainer>
         <div id="site-header__layout" class="flex justify-between items-center py-10">
           <div id="site-header__logo" class="mr-12">
             <img src="@/assets/sample_logo.png" width="100" height="20" />
-            <p class="font-medium uppercase text-xs" :style="{ color: color }">Logo description</p>
+            <p class="font-medium uppercase text-xs" :style="{ color }">Logo description</p>
           </div>
           <button
             id="site-header__menu"
